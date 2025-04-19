4fcd662c6d5e2c8f1de47889749b172450a63f83
伊藤 駿 <rendya2501@gmail.com>
2025-04-19 19:33:36 +0900

    DownloadLinkProcessor整理

--- src/Core/Services/Test/DownloadLinkProcessor.cs
+++ src/Core/Services/Test/DownloadLinkProcessor.cs
@@ -1,6 +1,5 @@
 using NotionMarkdownConverter.Core.Constants;
 using NotionMarkdownConverter.Infrastructure.Http.Services;
-using System;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -35,58 +34,6 @@ public class DownloadLinkProcessor(IFileDownloader _imageDownloader) : IDownload
     /// <returns>マークダウン</returns>
     public async Task<string> ProcessLinkAsync(string markdown, string outputDirectory)
     {
-        //// マークダウン内のダウンロードリンクを抽出
-        //var urls = ExtractDownloadLinks(markdown);
-
-        //// URLとファイル名のペアを生成
-        //var urlFilePairs = urls.Select(url =>
-        //{
-        //    var sanitizedUrl = SanitizeUrl(url);
-        //    return new UrlFilePair(sanitizedUrl, GenerateFileName(sanitizedUrl));
-        //});
-
-        //// ダウンロードタスクを生成
-        //var downloadTasks = urlFilePairs.Select(pair =>
-        //    _imageDownloader.DownloadAsync(pair, outputDirectory));
-
-        //// ダウンロードが完了するまで待機
-        //await Task.WhenAll(downloadTasks);
-
-        //// マークダウンのダウンロードリンクを置換
-        //return ReplaceUrls(markdown, urlFilePairs);
-
-
-
-
-        //// 1. マークダウン文字列から固定文字列をregexする
-        //var urls = ExtractDownloadLinks(markdown);
-
-        //// 2. regexした結果からローカルリンクを生成しておく
-        //var urlToLocalLinkPairs = urls.ToDictionary(
-        //    url => url,
-        //    url => GenerateFileName(SanitizeUrl(url))
-        //);
-
-        //// 3. マークダウン文字列からregexした結果とローカルリンクのペアを置換する
-        //var updatedMarkdown = ReplaceUrls(markdown, urlToLocalLinkPairs);
-
-        //// 4. サニタイジング文字列とローカルリンクのペアを元にダウンロードタスクを生成する
-        //var downloadTasks = urlToLocalLinkPairs.Select(pair =>
-        //{
-        //    var sanitizedUrl = SanitizeUrl(pair.Key);
-        //    var localFileName = pair.Value;
-        //    return _imageDownloader.DownloadAsync(new UrlFilePair(sanitizedUrl, localFileName), outputDirectory);
-        //});
-
-        //// 5. タスクをawaitする
-        //await Task.WhenAll(downloadTasks);
-
-        //// 6. 結果を返す
-        //return updatedMarkdown;
-
-
-
-
         // 1. マークダウン内のダウンロードリンクを抽出
         var urls = ExtractDownloadLinks(markdown);
 
@@ -97,7 +44,7 @@ public class DownloadLinkProcessor(IFileDownloader _imageDownloader) : IDownload
         );
 
         // 3. マークダウン文字列からregexした結果とローカルリンクのペアを置換する
-        Task<string> replaceTask = Task.Run(() => ReplaceUrls(markdown, urlToLocalLinkPairs));
+        Task<string> replaceTask = Task.Run(() => ReplaceDownloadLinks(markdown, urlToLocalLinkPairs));
 
         // 4. サニタイジング文字列とローカルリンクのペアを元にダウンロードタスクを生成する
         IEnumerable<Task> downloadTasks = urlToLocalLinkPairs.Select(pair =>
@@ -112,7 +59,6 @@ public class DownloadLinkProcessor(IFileDownloader _imageDownloader) : IDownload
 
         // 6. 結果を返す
         return await replaceTask;
-
     }
 
 
@@ -164,7 +110,7 @@ public class DownloadLinkProcessor(IFileDownloader _imageDownloader) : IDownload
     /// <param name="markdown"></param>
     /// <param name="urlFilePairs"></param>
     /// <returns></returns>
-    private static string ReplaceUrls(string markdown, Dictionary<string, string> urlToLocalLinkPairs)
+    private static string ReplaceDownloadLinks(string markdown, Dictionary<string, string> urlToLocalLinkPairs)
     {
         var result = markdown;
         foreach (var pair in urlToLocalLinkPairs)
@@ -173,13 +119,30 @@ public class DownloadLinkProcessor(IFileDownloader _imageDownloader) : IDownload
         }
         return result;
     }
-    //private static string ReplaceUrls(string markdown, IEnumerable<UrlFilePair> urlFilePairs)
-    //{
-    //    var result = markdown;
-    //    foreach (var file in urlFilePairs)
-    //    {
-    //        result = result.Replace(LinkConstants.DownloadMarker + file.OriginalUrl, file.ConversionFileName);
-    //    }
-    //    return result;
-    //}
 }
+//// 1. マークダウン文字列から固定文字列をregexする
+//var urls = ExtractDownloadLinks(markdown);
+
+//// 2. regexした結果からローカルリンクを生成しておく
+//var urlToLocalLinkPairs = urls.ToDictionary(
+//    url => url,
+//    url => GenerateFileName(SanitizeUrl(url))
+//);
+
+//// 3. マークダウン文字列からregexした結果とローカルリンクのペアを置換する
+//var updatedMarkdown = ReplaceUrls(markdown, urlToLocalLinkPairs);
+
+//// 4. サニタイジング文字列とローカルリンクのペアを元にダウンロードタスクを生成する
+//var downloadTasks = urlToLocalLinkPairs.Select(pair =>
+//{
+//    var sanitizedUrl = SanitizeUrl(pair.Key);
+//    var localFileName = pair.Value;
+//    return _imageDownloader.DownloadAsync(new UrlFilePair(sanitizedUrl, localFileName), outputDirectory);
+//});
+
+//// 5. タスクをawaitする
+//await Task.WhenAll(downloadTasks);
+
+//// 6. 結果を返す
+//return updatedMarkdown;
+

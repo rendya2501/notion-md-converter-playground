1d26192e7a5e30700b5cc8294c0784b1c2719c8b
伊藤 駿 <rendya2501@gmail.com>
2026-03-16 17:13:17 +0900

    refactor : プロパティを順番に変更

--- src/Shared/Constants/NotionPagePropertyNames.cs
+++ src/Shared/Constants/NotionPagePropertyNames.cs
@@ -5,27 +5,27 @@ namespace NotionMarkdownConverter.Shared.Constants;
 /// </summary>
 public static class NotionPagePropertyNames
 {
-    /// <summary>Notionページのタイトルプロパティ名</summary>
-    public const string TitlePropertyName = "Title";
-
-    /// <summary>記事タイプ（例: article, idea）を示すセレクトプロパティ名</summary>
-    public const string TypePropertyName = "Type";
-
     /// <summary>公開日時を示す日付プロパティ名</summary>
     public const string PublishedAtPropertyName = "PublishedAt";
-
-    /// <summary>最終クロール日時を記録するシステム管理プロパティ名</summary>
-    public const string CrawledAtPropertyName = "_SystemCrawledAt";
-
-    /// <summary>タグのマルチセレクトプロパティ名</summary>
-    public const string TagsPropertyName = "Tags";
-
-    /// <summary>記事の説明文を示すリッチテキストプロパティ名</summary>
-    public const string DescriptionPropertyName = "Description";
 
     /// <summary>URLスラグを示すリッチテキストプロパティ名</summary>
     public const string SlugPropertyName = "Slug";
 
+    /// <summary>Notionページのタイトルプロパティ名</summary>
+    public const string TitlePropertyName = "Title";
+
+    /// <summary>記事タイプ（例: Tech, Idea）を示すセレクトプロパティ名</summary>
+    public const string TypePropertyName = "Type";
+
+    /// <summary>タグのマルチセレクトプロパティ名</summary>
+    public const string TagsPropertyName = "Tags";
+
     /// <summary>公開ステータス（Published / Queued / Unpublished）を示すセレクトプロパティ名</summary>
     public const string PublicStatusName = "PublicStatus";
+
+    /// <summary>記事の説明文を示すリッチテキストプロパティ名</summary>
+    public const string DescriptionPropertyName = "Description";
+
+    /// <summary>最終クロール日時を記録するシステム管理プロパティ名</summary>
+    public const string CrawledAtPropertyName = "_SystemCrawledAt";
 }
